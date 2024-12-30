9c0964266fef5d8a3187f3f5f49782f39e06a932
527aboo <o.oba725@gmail.com>
2024-12-30 23:29:04 +0900

    tailwindcssの有効化

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
-"use client"
-import { signIn } from "next-auth/react";
+
+import Link from "next/link";
 
 export default function Login() {
     return (
-        <div>
-            <h1>ログイン</h1>
-            <button onClick={() => signIn("google")}>Googleでログイン</button>
-        </div>
+        <Link href={'/api/auth/signin'} 
+        className='text-gray-300 hover:text-white 
+        px-3 py-2 rounded-md text-sm font-medium'>
+        googleでログイン
+    </Link>   
     )
 }
