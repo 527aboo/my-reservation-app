f429be904a79dde52b972a25289b56aa503e5d51
527aboo <o.oba725@gmail.com>
2024-12-30 13:13:41 +0900

    ログインの処理を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NextAuthProvider from "@/lib/auth/provider";
+import Header from "./components/common/Header";
+import { Suspense } from "react";
+import Loading from "./components/common/Loading";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><NextAuthProvider>
+      <Header />
+      <Suspense fallback={<Loading />}>
         {children}
+      </Suspense>
+    </NextAuthProvider>
       </body>
     </html>
   );
