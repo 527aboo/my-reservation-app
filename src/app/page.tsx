bfc919f4d7eb9f857bb1af4adcadd7be425a8b0f
527aboo <o.oba725@gmail.com>
2025-01-02 10:44:36 +0900

    topページに予約画面を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { User } from "@/types/type";
+import ReservationForm from "./components/reservation/ReservationForm";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "@/lib/next-auth/option";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(nextAuthOptions);
+  const user: User = session?.user as User;
+
   return (
-    <div>
-      HOME
-    </div>
-  );
+      <ReservationForm user={user} />
+  )
 }
